9c94d65836bf5a5bb1f985c14e519786d38fb124
mjhubert <mjhubert@github.com>
2022-02-22 21:31:03 +0100

    Docker

--- TORDOCKER/start.sh
+++ TORDOCKER/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-nohup sudo -u debian-tor tor &
+sudo -u debian-tor tor &
 
+sleep 15
 while [ ! -f /var/lib/tor/tendermint/hostname ] ;
 do
       sleep 1
@@ -8,16 +9,19 @@ done
 
 onionaddress=`cat /var/lib/tor/tendermint/hostname`
 
-~/tendermint/build/tendermint init validator
+/root/tendermint/build/tendermint init validator
 
-nodeid=`~/tendermint/build/tendermint show-node-id`
+nodeid=`/root/tendermint/build/tendermint show-node-id`
 
 sed -i "s/external-address = \"\"/external-address = \"$onionaddress:26656\"/" /root/.tendermint/config/config.toml
 
 echo "$nodeid@$onionaddress:26656" > nodeaddress
+
+echo "-------------------------------------"
 cat nodeaddress
+echo "-------------------------------------"
 
-
+sleep infinity
 
 
 
